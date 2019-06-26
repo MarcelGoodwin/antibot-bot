857a0a3eee03ebff03c29f0d700df56c8ee5ed31
Marcel Goodwin <mp.goodwin98@gmail.com>
2019-06-25 23:38:13 -0400

    minor edit

--- antibot.py
+++ antibot.py
@@ -7,13 +7,15 @@ token = open("bot-token.txt", "r").read(59)
 
 client  = discord.Client() #start the discord Client
 
+
+
 @client.event
 async def on_ready():
     print(f'we have logged in as client {client.user}')
 
 @client.event
 async def on_message(message):
-    print(f'{message.channel}: {message.author}: {message.author.name}: {message.content}')
+    #print(f'{message.channel}: {message.author}: {message.author.name}: {message.content}')
     if message.content == "!antibot status":
         await message.channel.send("Antibot-bot is running")
 
