885d90e095ac999e28f218b1528e9031c1d33b59
Marcel Goodwin <mp.goodwin98@gmail.com>
2019-07-02 23:01:00 -0400

    multiple server ban lists supported. antibot doesn't self ban

--- antibot.py
+++ antibot.py
@@ -7,16 +7,37 @@ token = open("bot-token.txt", "r").read(59)
 
 client  = discord.Client() #start the discord Client
 
-
+connectedServers = {}
+banished = []
 
 @client.event
 async def on_ready():
     print(f'we have logged in as client {client.user}')
+    for server in client.guilds:
+        if server not in connectedServers:
+            connectedServers[server] = []
+
 
 @client.event
 async def on_message(message):
     #print(f'{message.channel}: {message.author}: {message.author.name}: {message.content}')
-    if message.content == "!antibot status":
-        await message.channel.send("Antibot-bot is running")
+    wordList  = message.content.split()
+    if message.author in connectedServers[message.guild]:
+        await message.delete()
+    else:
+        if message.content == "!antibot status":
+            await message.channel.send("Antibot-bot is running")
+        if wordList[0] == "a!banish":
+            username = wordList[1]
+            for x in range(2, len(wordList)):
+                username  = username + " " + wordList[x]
+            if username == "anti-bot bot":
+                await message.channel.send("Anti-bot refuses to banish itself. You hold no power over it.")
+            else:
+                for user in message.guild.members:
+                    if user.name == username and user not in connectedServers[message.guild] and user.bot:
+                        connectedServers[message.guild].append(user)
+                        await message.channel.send(user.name + " has been banished. Permanently.")
+
 
 client.run(token)
